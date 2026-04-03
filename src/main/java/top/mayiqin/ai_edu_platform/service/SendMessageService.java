4c713f39de992694e51b6c8708dae9378f48230f
aurora <16894557+aurora10124@user.noreply.gitee.com>
2026-04-03 11:39:28 +0800

    新建文件夹中

---
+++ src/main/java/top/mayiqin/ai_edu_platform/service/SendMessageService.java
@@ -0,0 +1,4 @@
+package top.mayiqin.ai_edu_platform.service;
+
+public interface SendMessageService {
+}
---
+++ src/main/java/top/mayiqin/ai_edu_platform/service/impl/SendMessageServiceImpl.java
@@ -0,0 +1,9 @@
+package top.mayiqin.ai_edu_platform.service.impl;
+
+import top.mayiqin.ai_edu_platform.service.SendMessageService;
+
+
+public class SendMessageServiceImpl implements SendMessageService {
+
+
+}
